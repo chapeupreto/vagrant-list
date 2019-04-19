fc68c06d236bc36b61924bcccae233802d73559d
Rod Elias <rodiney@gmail.com>
2019-04-19 13:07:46 -0300

    stop colouring output

--- vagrant-list.sh
+++ vagrant-list.sh
@@ -2,7 +2,7 @@
 
 which -s vagrant
 if [[ $? -ne 0 ]]; then
-    echo -e "\n\e[91mSorry! vagrant was not found!\n"
+    echo -e "\n\e[91mSorry! vagrant was not found!\e[0m\n"
     exit 1
 fi
 
@@ -12,7 +12,7 @@ clear && vagrant global-status --prune > "${temp_file}" 2> /dev/null
 
 num_lines=$(wc -l < "${temp_file}")
 
-[[ "${num_lines}" -eq 5 ]] && echo -e "\n\e[91mSorry! No vagrant machines running!\n" && rm -f "${temp_file}" && exit 1
+[[ "${num_lines}" -eq 5 ]] && echo -e "\n\e[91mSorry! No vagrant machines running!\e[0m\n" && rm -f "${temp_file}" && exit 1
 
 directories=$(head -$(("${num_lines}" - 7)) "${temp_file}" | tail +3 | awk '{ print $NF }')
 
